8aba48dbd01fcce0056c183b7267a011fabcf527
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-05 18:29:41 +0800

    Updated server-side test.

--- shft-test.js
+++ shft-test.js
@@ -2,26 +2,16 @@
 const assert = require( "assert" );
 const shft = require( "./shft.js" );
 
-assert.deepEqual( shft( true ), [ ], "should be deeply equal" );
-
-assert.deepEqual( shft( 123 ), [ ], "should be deeply equal" );
-
-assert.deepEqual( shft( { }, 2 ), [ ], "should be deeply equal" );
-
-let test1 = shft( [ { "greeting": "hi", "salutation": "dear" }, { "hello": "world" } ], 1 );
-assert.deepEqual( test1, [ { "hello": "world" } ], "should be deeply equal" );
-
-let test2 = shft( [ { "greeting": "hi", "salutation": "dear" }, { "hello": "world" } ], 2 );
-assert.deepEqual( test2, [ ], "should be deeply equal" );
-
-assert.deepEqual( shft( [ 1, 2, 3 ], 1 ), [ 2, 3 ], "should be deeply equal" );
-
-assert.deepEqual( shft( [ 1, 2, 3 ] ), [ 2, 3 ], "should be deeply equal" );
-
 assert.deepEqual( shft( [ 1, 2, 3 ] ), [ 2, 3 ], "should be deeply equal" );
 
 assert.deepEqual( shft( [ 1, 2, 3, 4, 5 ], 2 ), [ 3, 4, 5 ], "should be deeply equal" );
 
 assert.deepEqual( shft( [ 1, 2, 3, 4, 5 ], -2 ), [ 4, 5 ], "should be deeply equal" );
+
+( function( ){
+
+	assert.deepEqual( shft( arguments, 2 ), [ "hello", Symbol.for( "hi" ) ], "should be deeply equal" );
+
+} )( Symbol.for( "hello" ), 2, "hello", Symbol.for( "hi" ) );
 
 console.log( "ok" )
