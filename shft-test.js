3f97dc71eacd9a54b4f234a7716debefa6185cdc
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-24 16:30:30 +0800

    Updated test.

--- shft-test.js
+++ shft-test.js
@@ -2,15 +2,15 @@
 const assert = require( "assert" );
 const shft = require( "./shft.js" );
 
-assert.deepEqual( shft( [ 1, 2, 3 ] ), [ 2, 3 ], "should be deeply equal" );
+assert.deepEqual( shft( [ 1, 2, 3 ] ), [ 2, 3 ], "should return [ 2, 3 ]" );
 
-assert.deepEqual( shft( [ 1, 2, 3, 4, 5 ], 2 ), [ 3, 4, 5 ], "should be deeply equal" );
+assert.deepEqual( shft( [ 1, 2, 3, 4, 5 ], 2 ), [ 3, 4, 5 ], "should return [ 3, 4, 5 ]" );
 
-assert.deepEqual( shft( [ 1, 2, 3, 4, 5 ], -2 ), [ 4, 5 ], "should be deeply equal" );
+assert.deepEqual( shft( [ 1, 2, 3, 4, 5 ], -2 ), [ 4, 5 ], "should return [ 4, 5 ]" );
 
 ( function( ){
 
-	assert.deepEqual( shft( arguments, 2 ), [ "hello", Symbol.for( "hi" ) ], "should be deeply equal" );
+	assert.deepEqual( shft( arguments, 2 ), [ "hello", Symbol.for( "hi" ) ], "should return [ 'hello', Symbol.for( 'hi' ) ]" );
 
 } )( Symbol.for( "hello" ), 2, "hello", Symbol.for( "hi" ) );
 
